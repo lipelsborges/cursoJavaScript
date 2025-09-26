8cbe9d26b0fd7f1ad1e165c97a6f03ca2ffa5e19
Felipe Borges <lipelsborges@gmail.com>
2025-09-26 17:12:10 -0300

    aa

---
+++ aula7/exercicio.js
@@ -0,0 +1,11 @@
+const nome = "Luiz Otávio"
+const sobrenome = "Miranda"
+const idade = 30;
+const kg = 84;
+const altura = 1.8;
+const IMC =  kg / (altura * altura);
+const anoNascimento = 1980; 
+
+console.log(nome, sobrenome,"tem", idade, "anos, pesa",kg,"kg");
+console.log("tem", altura, "de altura e seu IMC é de",IMC, "!")
+console.log(nome, "nasceu em", anoNascimento, "!") 
