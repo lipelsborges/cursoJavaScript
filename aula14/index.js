680c8807d8eb22ec275b95a3769409d12668e1d9
Felipe Borges <lipelsborges@gmail.com>
2025-09-27 14:42:44 -0300

    sss

---
+++ aula14/index.js
@@ -0,0 +1,7 @@
+let num1 = 0.7;
+let num2 = 0.1;
+
+num3 = num1 + num2 
+
+num4 = (num3.toFixed(2));
+console.log(num4)
---
+++ aula14/math.js
@@ -0,0 +1,9 @@
+let num1 = 9.54578;
+let num2 = Math.floor(num1); /// Math.ceil arredonda pra cima //
+//let num = Math.round(num1) /// arredonda pro numero mais perto
+// Math.max(1, 2 , 3, 4 , 10) pega o maior
+// Math.min(1, 2 , 3, 4 , 10) pega o menor
+// math.random numero aleatorio
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5); // numero aleatorio de 5 a 10
+console.log(num2);
+
