a466e263243a5e2fe73026b18a9228174314b292
Felipe Borges <lipelsborges@gmail.com>
2025-09-27 17:56:58 -0300

    Update index.js

--- aula16/index.js
+++ aula16/index.js
@@ -2,7 +2,9 @@ const alunos = ['Felipe', 'Leonardo', 'Borges']
 
 //alunos.unshift('') para adicionar em primeiro
     //alunos.push('') para adicionar em ultimo
-
+        // alunos.pop() remove o ultimo
+        // alunos.shift() para remover o primeiro
+         // const removido = alunos.pop --- para criar uma variavel
 //console.log(alunos)
 //console.log(alunos[0]);
 //console.log(alunos[2]);
