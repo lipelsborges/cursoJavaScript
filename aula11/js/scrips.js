3101ca651bb9ffe2db4ca2914f5da7100c859650
Felipe Borges <lipelsborges@gmail.com>
2025-09-27 12:23:44 -0300

    aa

--- aula11/js/scrips.js
+++ aula11/js/scrips.js
@@ -1,1 +1,6 @@
-alert('')
+const num1 = Number(prompt('Digite um numero'));
+const num2 = Number(prompt('Digite outro numero'));
+
+const resultado = num1 + num2
+
+alert(resultado)
---
+++ aula12/ex.js
@@ -0,0 +1,7 @@
+let varA = "A";
+let varB = "B";
+let varC = "C";
+
+
+let vars = [varA, varB, varC];
+console.log(vars[1], vars[2], vars[0]);
