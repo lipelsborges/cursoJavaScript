efee539e49addccd2a796be5ec63fd20a72ae2d3
Felipe Borges <lipelsborges@gmail.com>
2025-09-27 17:55:10 -0300

    Create index.js

---
+++ aula16/index.js
@@ -0,0 +1,8 @@
+const alunos = ['Felipe', 'Leonardo', 'Borges']
+
+//alunos.unshift('') para adicionar em primeiro
+    //alunos.push('') para adicionar em ultimo
+
+//console.log(alunos)
+//console.log(alunos[0]);
+//console.log(alunos[2]);
