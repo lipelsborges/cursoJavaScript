a8e40b22f3e63e92c9a0ccbcfc9c461acdd0e37e
Felipe Borges <lipelsborges@gmail.com>
2025-09-29 14:39:16 -0300

    aa

--- aula19/index.js
+++ aula19/index.js
@@ -1,11 +1,29 @@
 function meuEscopo(){
     const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado')
 
-    form.onsubmit = function (evento){
-        evento
-        alert(1);
-        console.log("foi enviado.")
+    const pessoas = [];
 
-    };
+    function recebeEventoForm(evento){
+        evento.preventDefault();
+        const nome =form.querySelector('.nome');
+        const sobrenome =form.querySelector('.sobrenome');
+        const peso =form.querySelector('.peso');
+        const altura =form.querySelector('.altura');
+
+        pessoas.push[{
+            nome,
+            sobrenome,
+            peso,
+            altura,
+        }];
+        console.log(pessoas);
+    }
+    resultado.innerHTML += (nome.value);
+    resultado.innerHTML += (sobrenome.value);
+    resultado.innerHTML += (peso.value);
+    texto.innerHTML += (altura.value);
+ 
+   form.addEventListener('submit', recebeEventoForm);
 }
 meuEscopo();
