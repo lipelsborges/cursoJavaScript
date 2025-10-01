73c34467383d28c47fd8124569d1b00bd224ed0c
Felipe Borges <lipelsborges@gmail.com>
2025-09-30 22:17:06 -0300

    aa

---
+++ aula27/index.js
@@ -0,0 +1,3 @@
+const pontUsuario = 500;
+const nivelUsuario = pontUsuario >= 1000 ? 'Usuario vip' : 'Usuario normal';
+console.log(nivelUsuario)
---
+++ aula28/index.js
@@ -0,0 +1,26 @@
+/*
+
+const tresHoras = 60 * 60 * 3 * 1000;
+const data = new Date(0 + tresHoras);
+console.log(data.toString());
+
+*/
+
+
+function zeroAEsquerda (num){
+    return num >= 10 ? num : `0${num}`;
+}
+
+function formataData(data){
+    const dia = zeroAEsquerda(data.getDate());
+    const mes = zeroAEsquerda(data.getMonth());
+    const ano = zeroAEsquerda(data.getFullYear());
+    const hora = zeroAEsquerda(data.getHours());
+    const min = zeroAEsquerda(data.getMinutes());
+    const seg = zeroAEsquerda(data.getSeconds());
+
+    return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`;
+}
+const data = new Date();
+const dataBrasil = formataData(data);
+console.log(dataBrasil);
