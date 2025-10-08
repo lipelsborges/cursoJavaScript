b4d31f7691464759217afa32074544d888a90527
Felipe Borges <lipelsborges@gmail.com>
2025-10-08 16:13:05 -0300

    a

---
+++ aula49/index.js
@@ -0,0 +1,25 @@
+// argumentos que sustenta todos os argumentos enviados
+function funcao() {
+  let total = 0;
+  for (let argumento of arguments) {
+    total += argumento;
+  }
+
+  console.log(total);
+}
+
+funcao(1, 2, 3, 4, 5, 6, 7);
+
+
+/* const conta = function(operador, acumulador, ...numeros) {
+  for (let numero of numeros) {
+    if (operador === '+') acumulador += numero;
+    if (operador === '-') acumulador -= numero;
+    if (operador === '/') acumulador /= numero;
+    if (operador === '*') acumulador *= numero;
+  }
+
+  console.log(acumulador);
+};
+
+conta('+', 1, 20, 30, 40, 50); */
