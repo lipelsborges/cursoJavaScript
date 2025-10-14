0d77bbb91f540332bd61e7ab47ee31cbadc3b574
Felipe Borges <lipelsborges@gmail.com>
2025-10-14 15:14:28 -0300

    Update index.js

--- aula70/index.js
+++ aula70/index.js
@@ -11,3 +11,34 @@ pessoa1.getDataNascimento = function() {
 };
 
 console.log(pessoa1.getDataNascimento());
+
+
+// factory functions
+
+/* function criaPessoa(nome, sobrenome) {
+    return {
+        nome,
+        sobrenome,
+        nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`;
+        }
+    };
+}
+
+const p1 = criaPessoa('Luiz', 'Otávio');
+console.log(p1.nomeCompleto); */
+
+
+
+/*
+constructor functions 
+function Pessoa(nome, sobrenome) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+}
+
+// {} <- this -> this
+const p1 = new Pessoa('Luiz', 'Miranda');
+const p2 = new Pessoa('Maria', 'Miranda');
+console.log(p1);
+*/
