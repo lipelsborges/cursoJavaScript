dd5ec8882223cb1a91d368c4195a7b4bb72709b6
Felipe Borges <lipelsborges@gmail.com>
2025-10-14 15:05:17 -0300

    Create index.js

---
+++ aula70/index.js
@@ -0,0 +1,13 @@
+const pessoa1 = new Object();
+pessoa1.nome = 'Luiz';
+pessoa1.sobrenome = 'Otávio';
+pessoa1.idade = 2;
+pessoa1.falarNome = function() {
+    return (`${this.nome} está falando seu nome.`);
+};
+pessoa1.getDataNascimento = function() {
+    const dataAtual = new Date();
+    return dataAtual.getFullYear() - this.idade;
+};
+
+console.log(pessoa1.getDataNascimento());
